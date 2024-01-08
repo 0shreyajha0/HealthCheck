73bf87d1af0a959eddfc738eb7d7ef1edd9515fb
Shreya jha <shreya_en20a18_13@dtu.ac.in>
2024-01-08 23:56:05 +0530

    Project initialised

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Header from "./Components/Header";
+import Footer from "./components/Footer";
+import MainSection from "./Components/MainSection";
+import PopUp from "./Components/PopUp";
 
-function App() {
+const App = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const storedUserName = localStorage.getItem("userName");
+    if (!storedUserName) {
+      setShowPopup(true);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      {showPopup && <Popup />}
+
+      <main>
+        <MainSection />
+      </main>
+
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
